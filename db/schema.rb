5731ebfd0732420ea7167f3f0d1ffebed8eff160
Andrew Ek <andrewek@gmail.com>
2022-09-22 14:36:17 -0500

    Initial category model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_190627) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_193536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
