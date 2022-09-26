a2b4f7779ae2ab8b3c1d79bb0167637b51b8c69d
Andrew Ek <andrewek@gmail.com>
2022-09-26 09:43:12 -0500

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ salad = Category.create!(name: "Salads")
 pasta = Category.create!(name: "Pastas")
 dessert = Category.create!(name: "Desserts")
 
-Recipe.create!(name: "Caesar Salad", duration_in_minutes: 20, category_id: salad.id)
-Recipe.create!(name: "Spaghetti", duration_in_minutes: 45, category_id: pasta.id)
-Recipe.create!(name: "Torte", duration_in_minutes: 90, category_id: dessert.id)
+julia = Author.create!(name: "Julia Turshen")
+angela = Author.create!(name: "Angela Garbacz")
+
+Recipe.create!(name: "Caesar Salad", duration_in_minutes: 20, category_id: salad.id, author_id: julia.id)
+Recipe.create!(name: "Spaghetti", duration_in_minutes: 45, category_id: pasta.id, author_id: julia.id)
+Recipe.create!(name: "Torte", duration_in_minutes: 90, category_id: dessert.id, author_id: angela.id)
