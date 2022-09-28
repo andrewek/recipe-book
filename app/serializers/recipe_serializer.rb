9a6fdee3499e183080457323eea0d031bc3e3c1a
Andrew Ek <andrewek@gmail.com>
2022-09-28 15:44:10 -0500

    Add recipe serializer schema annotations

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id                  :bigint           not null, primary key
+#  duration_in_minutes :integer          default(30), not null
+#  name                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  author_id           :bigint
+#  category_id         :bigint
+#
+# Indexes
+#
+#  index_recipes_on_author_id    (author_id)
+#  index_recipes_on_category_id  (category_id)
+#
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :duration_in_minutes, :author, :category
   has_many :tags
