1c923936b114cd70a08a0391ea17887f7c8c5f7a
Andrew Ek <andrewek@gmail.com>
2022-10-04 10:07:34 -0500

    Add tags factory, fix tag model spec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -10,45 +10,27 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  let(:author) { Author.create!(name: "Gordon Ramsay") }
-  let(:category) { Category.create!(name: "Fancy Food") }
-  let(:recipe) do
-    Recipe.create!(
-      name: "Filet Mignon",
-      author: author,
-      duration_in_minutes: 120,
-      category: category
-    )
-  end
-  let(:tag) { Tag.new(name: "comfort food") }
-
   describe "#valid?" do
     it "validates presence of name" do
-      tag.name = nil
+      tag = build(:tag, name: nil)
 
       expect(tag).not_to be_valid
       expect(tag.errors[:name]).to include("is too short (minimum is 2 characters)")
     end
 
     it "validates length of name" do
-      tag.name = "a"
+      tag = build(:tag, name: "a")
 
       expect(tag).not_to be_valid
       expect(tag.errors[:name]).to include("is too short (minimum is 2 characters)")
-    end
-
-    it "allows lacking recipe association" do
-      tag.recipes = []
-      
-      expect(tag).to be_valid
     end
   end
 
   describe "#name_is_like" do
     it "finds tags by name" do
-      t_1 = Tag.create!(name: "easy to make")
-      t_2 = Tag.create!(name: "southern style cooking")
-      t_3 = Tag.create!(name: "hard to make")
+      t_1 = create(:tag, name: "easy to make")
+      t_2 = create(:tag, name: "southern style cooking")
+      t_3 = create(:tag, name: "hard to make")
 
       result = Tag.name_is_like("make")
 
